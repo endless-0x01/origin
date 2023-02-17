25f892cfa0de0c0dd6c4ebbc21b1d4007c2176de
Gleb <gleborel22@mail.ru>
2023-02-17 20:34:37 +0300

    Added all tasks for Compiling and linking under the hood

---
+++ Compiling and linking under the hood/task 1/main.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+// было объявлено пространство имен, я убрал, так как считаю это плохой практикой =)
+
+struct point {
+	double m_x;
+	double m_y;
+	point() = default;
+	point(double x, double y) {
+		m_x = x;
+		m_y = y;
+	}
+}; // не было точки с запятой
+
+void print_point(const point& point_object); // было два определения, сделал объявления
+
+int main()
+{
+	// не понял зачем заводить не нужную переменную , когда ее можно объявить в цикле
+	for (int i = 0; i < 5; i++)
+	{
+		point my_point(i, i * 2);
+		print_point(my_point);
+	}
+}
+
+void print_point(const point& point_object) {
+	std::cout << "x:" << point_object.m_x << ", y: "
+		<< point_object.m_y << std::endl;
+}
---
+++ Compiling and linking under the hood/task 2/main.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+
+// ошибка линвковки - компилятор не может понять какую функцию ему вызвать, решением будет 
+// сделать объвления int func(int); либо перегрузка функции
+int func(int a) {
+	return a * a;
+}
+int func(int a)
+{
+	return a * a;
+}
+
+int main()
+{
+	// синтаксическая ошибка
+	std::cout << Hello World!<< std::endl; // не хватает кавычек
+	//std::cout << "Hello World!" << std::endl;
+
+	// семантическая ошибка, так как i никогда не будет больше 10 
+	for (int i = 0; i > 10; i++) // правильный вариант i < 10
+	{
+		std::cout << i << std::endl;
+	}
+
+	std::cout << func(4);
+
+
+	return 0;
+}
