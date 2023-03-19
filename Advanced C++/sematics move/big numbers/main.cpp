35d4a2dc0d4ef51c6b9ddb4020055096d69e1db7
Gleb <gleborel22@mail.ru>
2023-03-19 17:06:50 +0300

    Fix task bigint

--- Advanced C++/sematics move/big numbers/main.cpp
+++ Advanced C++/sematics move/big numbers/main.cpp
@@ -1,39 +1,95 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 
 struct big_intenger
 {
 public:
-	big_intenger(std::string num)
-	{
-		number = std::atoll(num.c_str());
-	}
+	big_intenger(std::string num) : number{ num } {}
 
 	big_intenger(big_intenger&& other) noexcept :
 		number{other.number}
 	{
-		other.number = 0;
+		other.number = "0";
 	}
 	big_intenger& operator=(big_intenger&& other) noexcept {
 		if (this == &other) return *this;
 		number = other.number;
-		other.number = 0;
+		other.number = "0";
 	}
 
-	big_intenger operator+(const big_intenger& other) {
-		return big_intenger(std::to_string(number + other.number));
+	big_intenger operator+(big_intenger& other) {
+
+		number.length() < other.number.length() ?
+			stand_null(number, number.length() - other.number.length()) : stand_null(other.number, number.length() - other.number.length());
+		std::string result;
+		for (size_t i = 0; i < number.length(); i++) {
+
+
+			int res = (number[i] - '0') + (other.number[i] - '0');
+			if (res > 9) {
+				res = res % 10;
+				result += std::to_string(res);
+				int index = i - 1;
+				int h_r = (result[index] - '0') + 1;
+				result[index] = h_r + '0';
+			}
+			else
+			{
+				result += std::to_string(res);
+			}
+
+		}
+
+		return { result };
 	}
 
-	big_intenger operator*(const big_intenger& other) {
-		return big_intenger(std::to_string(number * other.number));
+
+	big_intenger operator*(big_intenger& other) {
+
+		number.length() < other.number.length() ?
+			stand_null(number, number.length() - other.number.length()) : stand_null(other.number, number.length() - other.number.length());
+		std::string result;
+
+		for (size_t i = 0; i <= number.length(); i++) {
+
+
+
+			int res = (number[i] - '0') * (other.number[i] - '0');
+			if (res > 9) {
+				res = res % 10;
+				result += std::to_string(res);
+				int index = i - 1;
+				int h_r = (result[index] - '0') + 1;
+				result[index] = h_r + '0';
+			}
+			else
+			{
+				result += std::to_string(res);
+			}
+
+		}
+		return { result };
 	}
 
-	const long long get_num() const{
+
+	const std::string get_num() const{
 		return number;
 	}
 
+	const size_t get_size() {
+		return number.length();
+	}
+
 private:
-	long long number;
+	void stand_null(std::string& num, int different) {
+
+		for (int i = 0; i < different; i++) {
+			num = '0' + num;
+		}
+	}
+
+	std::string number;
 };
 
 std::ostream& operator<<(std::ostream& os, const big_intenger& object)
@@ -43,10 +99,16 @@ std::ostream& operator<<(std::ostream& os, const big_intenger& object)
 
 int main()
 {
+
 	auto number1 = big_intenger("114575");
 	auto number2 = big_intenger("78524");
 	auto result = number1 + number2;
 	std::cout << result; // 193099
+
+	auto number3 = big_intenger("2");
+	auto number4 = big_intenger("10");
+	auto result5 = number3 * number4;
+	std::cout << result5; // 20
 
 	return 0;
 }
--- Advanced C++/sematics move/move object/main.cpp
+++ Advanced C++/sematics move/move object/main.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 
 template<typename T>
-void move_vectors(std::vector<T>& object, std::vector<T>& object2)
+void move_vectors(std::vector<T>&& object, std::vector<T>& object2)
 {
 	object2 = object;
 	object.clear();
@@ -13,7 +13,7 @@ int main()
 
 	std::vector <std::string> one = { "test_string1", "test_string2" };
 	std::vector <std::string> two;
-	move_vectors(one, two);
+	move_vectors(std::move(one), two);
 	
 
 	return 0;
