2a69f9c6393b3d237135122aff8b72e1feb9f187
Gleb <gleborel22@mail.ru>
2023-07-13 23:46:29 +0300

    fix

--- Qt/CoreQtModules/QtModules/main.cpp
+++ Qt/CoreQtModules/QtModules/main.cpp
@@ -1,15 +1,13 @@
-#include <QApplication>
 #include <QtNetwork/QNetworkAccessManager>
 #include <QtSql/QSqlDatabase>
 #include <iostream>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
     QNetworkAccessManager networkManager;
 
     QSqlDatabase database = QSqlDatabase::addDatabase("QSQL");
 
     std::cout << "object created" << std::endl;
-    return a.exec();
+    return 0;
 }
